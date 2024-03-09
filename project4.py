ae47887c6541dd7a3f8c507787cf948fa8646706
Umar Hayat Khan Lodhi <lodhiumar2023@gmail.com>
2024-03-09 16:35:42 +0500

    Project 4

--- project4.py
+++ project4.py
@@ -1,1 +1,7 @@
-FINDING AREA OF TRIANGLE
+# FINDING AREA OF TRIANGLE (0.5)*height*base
+
+height = float(input("Please Enter Height: "))
+base = float(input("Please Enter Base: "))
+area = (0.5)*height*base
+
+print("Area of Triangle is: ", area)
